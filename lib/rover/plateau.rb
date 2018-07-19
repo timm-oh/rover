2a7ebb3eb8f0968294aeaf4f7bb425ba0beddaca
Tim McCarthy <omegoshi@gmail.com>
2018-07-19 22:03:11 +0200

    change: move exception to be called from another method

--- lib/rover/plateau.rb
+++ lib/rover/plateau.rb
@@ -1,14 +1,20 @@
 module Rover
   class Plateau
     def initialize(upper_bound_x = 5, upper_bound_y = 5)
-      raise IllegalArguementError unless upper_bound_x > 0 && upper_bound_y > 0
       @upper_bound_x = upper_bound_x
       @upper_bound_y = upper_bound_y
     end
 
+    def valid?
+      @upper_bound_x > 0 && @upper_bound_y > 0
+    end
+
     def valid_position?(position)
+      raise InvalidPlateauError unless valid?
       valid_x_position(position.x) && valid_y_position(position.y)
     end
+
+    class InvalidPlateauError < StandardError; end
 
     private
 
